c2e6e14f8ffc805b96a7856fce1392841231e916
Jooahn Cheung <70905309+jooahncheung@users.noreply.github.com>
2026-02-21 09:29:59 -0500

    Implement getComputerChoice function to randomly select rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World, testing if the script is working!");
+function getComputerChoice() {
+  draw = Math.floor(Math.random() * 3);
+  if (draw === 0) {
+    draw = "rock";
+  } else if (draw === 1) {
+    draw = "paper";
+  } else {
+    draw = "scissors";
+  }
+  return draw;
+}
