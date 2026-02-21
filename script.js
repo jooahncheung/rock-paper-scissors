4369d0a596e64509ebe602afba4dbbe57cb3fa84
Jooahn Cheung <70905309+jooahncheung@users.noreply.github.com>
2026-02-21 10:10:06 -0500

    Implement playRound function to determine game outcome and update scores

--- script.js
+++ script.js
@@ -21,4 +21,28 @@ function getHumanChoice() {
   return humanDraw.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+  humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie! Both chose " + humanChoice);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scsissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+  } else {
+    computerScore++;
+    console.log("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+// testing
 console.log(getHumanChoice());
