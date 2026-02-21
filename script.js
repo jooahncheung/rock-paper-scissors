bd696bd5ffc8ac55946b90fbb0891900e3b4aea8
Jooahn Cheung <70905309+jooahncheung@users.noreply.github.com>
2026-02-21 09:55:24 -0500

    Add score tracking and refine human choice prompt

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// global variable for scores
+let humanScore = 0;
+let computerScore = 0;
+
+// returns the computer's choice
 function getComputerChoice() {
   let computerDraw = Math.floor(Math.random() * 3);
   if (computerDraw === 0) {
@@ -10,8 +15,10 @@ function getComputerChoice() {
   return computerDraw;
 }
 
+// returns the human's choice
 function getHumanChoice() {
-  let humanDraw = window.prompt("What do you draw?").toLowerCase();
-  return humanDraw;
+  let humanDraw = window.prompt("Enter rock, paper, or scissors:");
+  return humanDraw.toLowerCase();
 }
+
 console.log(getHumanChoice());
